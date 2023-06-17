a7cc5190ea434a9187b92c6d1ec5abd7923d555a
artem <lyalin.vorota@mail.ru>
2023-06-17 20:20:31 +1000

    Задание 2

---
+++ D2.py
@@ -0,0 +1,5 @@
+s = int(input('Введите общее количество журавликов:'))
+Katy = s // 2
+Pety = Katy // 2
+Sergey = Pety
+print('Katy сделала ', Katy,','' '  'Pety сделал ', Pety,','' '  'Sergey сделал ', Sergey)
